0eb2a652319483d710d749e103128d8447771753
Kevin Squire <kevin.squire@gmail.com>
2013-03-08 18:07:26 -0800

    Updated README.md, tests

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 OrderedCollections
 ==================
 
+An ordered collection is simply a collection which has a particular
+order.  This package follows the convention where the default order is
+the insertion order of items in the collection, although this order
+can be changed via dequeue-like operations or sorting. 
+
 OrderedDict
------------
-`OrderedDict` here is an `Associative` which retains the insertion order of elements.  New
-key-value pairs are appended.  If a key from a key-value pair
-already exists in the dictionary, the original value is updated, and
-the key remains in its original position.
+===========
+`OrderedDict` here is an `Associative` which retains the insertion
+order of elements.  New key-value pairs are appended.  If a key from a
+key-value pair already exists in the dictionary, the original value is
+updated, and the key remains in its original position.
 
 In addition, many `Dequeue`-related functions are available:
 
@@ -19,8 +24,8 @@ In addition, many `Dequeue`-related functions are available:
                        # the dictionary
 ```
 
-Note also that this is not necessarily a sorted dictionary, although it can be
-sorted with 
+Note also that this is not necessarily a sorted dictionary, although
+it can be sorted with
 
 ```julia
   sort!(od)              # od is an OrderedDict()
@@ -36,6 +41,15 @@ back:
   #sort!(d)            # error! Dicts can't be sorted in place!
 ```
 
+Implementation
+--------------
+`OrderedDict` was implemented in a brute force manner: by copying and
+modifying Julia's `base/dict.jl`.  Various other methods of wrapping
+Dict were tried and discarded for performance reasons.
+
+This version has about a 40% insertion and 10% deletion slowdown
+compared with Dict().
+
 OrderedSet
-----------
+==========
 (In progress...)
--- test/dict_tests.jl
+++ test/dict_tests.jl
@@ -1,7 +1,5 @@
-
-using Benchmark
-require("OrderedDict.jl")
-require("OrderedDictAlt.jl")
+#using Benchmark
+using OrderedCollections
 
 n = 100000
 strs = [randstring(10) for i = 1:n];
@@ -69,61 +67,13 @@ function test_del(T::Type, iter::Int)
    t
 end
 
-d = Dict{String,Int}()
-od=OrderedDict{String,Int}()
-oda=OrderedDictAlt{String,Int}()
-
-       
-compare([()->dict_insertion_test(d), ()->dict_insertion_test(od3)], int(1000000/n))
-#compare([()->dict_insertion_test(d), ()->dict_insertion_test(od), ()->dict_insertion_test(od3)], int(1000000/n))
-#
-# n = 1_000_000
-#
-#            Function Elapsed Relative Replications
-#[1,]    "# function" 2.43333      1.0            1
-#[2,]    "# function" 3.13963  1.29026            1
-#[3,]    "# function" 5.05498   2.0774            1
-###########
-# n = 100_000
-#
-#            Function  Elapsed Relative Replications
-#[1,]    "# function" 0.593007      1.0          100
-#[2,]    "# function" 0.881763  1.48694          100
-#[3,]    "# function"  1.24341  2.09679          100
-
-
-#compare([()->dict_ins_del_test(d,n), ()->dict_ins_del_test(od,n), ()->dict_ins_del_test(od3,n)], int(100000/n))
-compare([()->dict_ins_del_test(d,n), ()->dict_ins_del_test(od3,n)], int(100000/n))
-#
-# n = 1_000_000
-#
-#            Function Elapsed Relative Replications
-#[1,]    "# function" 4.72177      1.0            1
-#[2,]    "# function" 57.4363  12.1641            1
-#[3,]    "# function" 38.0523  8.05891            1
-###########
-# n = 100_000
-#
-#            Function  Elapsed Relative Replications
-#[1,]    "# function" 0.078143      1.0           10
-#[2,]    "# function"  5.26876  67.4246           10
-#[3,]    "# function"  2.62656  33.6122           10
-
-
-compare([()->dict_deletion_test(d,n), ()->dict_deletion_test(od3,n)], int(1000000/n))
-#compare([()->dict_deletion_test(d,n), ()->dict_deletion_test(od,n), ()->dict_deletion_test(od3,n)], int(1000000/n))
-#
-# n = 1_000_000
-#
-#[1,]    "# function" 0.16877      1.0            1
-#[2,]    "# function" 10.2882  60.9598            1
-#[3,]    "# function" 8.07296  47.8342            1
-###########
-# n = 100_000
-#
-#            Function   Elapsed Relative Replications
-#[1,]    "# function" 0.0640104      1.0           10
-#[2,]    "# function"    10.208  159.475           10
-#[3,]    "# function"   9.93042  155.138           10
-
-
+for test in [test_ins, test_del, test_ins_del]
+    println(test)
+    println("="^length(string(test)))
+    for T in [Dict, OrderedDict]
+         print("$T: ")
+         times = Float64[test(T, 5) for i = 1:5]
+         println("$times, median=$(median(times))")
+    end
+    println()
+end
