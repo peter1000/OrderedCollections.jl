434b6b216d05bde4e626457108d6c0fa31467c60
Kevin Squire <kevin.squire@gmail.com>
2013-03-08 18:12:44 -0800

    Disabled OrderedSet until possible upstream patches are explored

--- src/OrderedSet.jl
+++ src/OrderedSet.jl
@@ -4,9 +4,9 @@
 # This is basic ordered set based on OrderedDicts
 #
 
-type OrderedSet{T}
-    hash::OrderedDict{T,Bool}
-    
-    OrderedSet() = new(OrderedDict{T,Bool}())
-    OrderedSet(x...) = append!(new(OrderedDict{T,Bool}(), x))
-end
+#type OrderedSet{T}
+#    hash::OrderedDict{T,Bool}
+#    
+#    OrderedSet() = new(OrderedDict{T,Bool}())
+#    OrderedSet(x...) = append!(new(OrderedDict{T,Bool}(), x))
+#end
